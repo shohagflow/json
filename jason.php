bf7a59ae09508f5421ec2b4d4634b9f84998724c
shohagflow <cseshohag1275@gmail.com>
2024-09-06 20:46:23 +0600

    it is a jason file

---
+++ jason.php
@@ -0,0 +1,18 @@
+<script>
+    const myData = {
+        name :'shohag'.
+        age : 30,
+        city :' Sirajganj',
+    }
+
+</script>
+<?php
+$myData = [
+    "name" => "jone",
+    "age" => 20,
+    "city" => "Dhaka"
+];
+echo json_encode($myData);
+
+
+?>
